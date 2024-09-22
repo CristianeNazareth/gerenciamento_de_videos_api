e402dbb3fb7d0ff6203c46b4043f6ac4a51c54f3
Cristiane Nazareth <cristianenazareth24@gmail.com>
2024-09-22 16:49:19 -0300

    vulnerability update

--- db/db.js
+++ db/db.js
@@ -1,23 +1,76 @@
 
-import pkg from 'pg';
-const { Pool } = pkg;
+// import pkg from 'pg';
+// const { Pool } = pkg;
+
+// const pool = new Pool({
+//   user: 'nazareth',
+//   host: 'localhost',
+//   database: 'videos_db',
+//   password: '241087',
+//   port: 5432,
+// });
+
+// pool.connect((err, client, release) => {
+//   if (err) {
+//     return console.error('Erro ao fazer a conexão:', err.stack);
+//   }
+//   console.log('Conexão concluído');
+
+//   release();
+// });
+
+
+// export default pool;
+
+
+
+
+
+
+// import { Pool } from 'pg';
+
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false
+//   }
+// });
+
+// pool.connect((err, client, release) => {
+//   if (err) {
+//     return console.error('Erro ao fazer a conexão:', err.stack);
+//   }
+//   console.log('Conexão concluída');
+
+//   release();
+// });
+
+// export default pool;
+
+
+
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+// Carregar variáveis de ambiente do .env
+dotenv.config();
 
 const pool = new Pool({
-  user: 'nazareth',
-  host: 'localhost',
-  database: 'videos_db',
-  password: '241087',
-  port: 5432,
+  connectionString: process.env.DATABASE_URL || 'postgresql://nazareth:241087@localhost:5432/videos_db',
+  ssl: {
+    rejectUnauthorized: false // Isso pode ser necessário para conexões seguras
+  }
 });
 
 pool.connect((err, client, release) => {
   if (err) {
     return console.error('Erro ao fazer a conexão:', err.stack);
   }
-  console.log('Conexão concluído');
+  console.log('Conexão concluída');
 
   release();
 });
 
-
 export default pool;
+
+
